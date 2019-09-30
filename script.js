dad4aa72ddf34bab2464d317aa8521cc229d9b60
CBlockSurprise <CBlockSurprise@users.noreply.github.com>
2019-09-30 13:01:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,30 +5,37 @@ var zeros = {
   b: new ComplexNumber(-0.5,-(Math.sqrt(3)/2))
 }
 
+var zoom = 150;
+var resolution = 500;
+
+var offset = {
+  x: 0,
+  y: 0
+}
+
 function setup() {
   createCanvas(500,500);
   colorMode(HSB, 100);
   noStroke()
+  drawTheFractal(); 
+}
+
+function drawTheFractal() {
   
-  //let lastNumber = new ComplexNumber(1,0);
-  //fill screen with pixels of certian color
-  //let x = new ComplexNumber(0,0);
-  for (let i = -250; i < 250; i++) {
-    for (let j = -250; j < 250; j++) {
+  for (let i = -250+offset.x; i < 250+offset.x; i++) {
+    for (let j = -250+offset.y; j < 250+offset.y; j++) {
       let f = new Polynomal([1,0,0,-1]);
       let fp = new Polynomal(f.difr());
-      let ap;
-      let pa;
       let nxii = 0;
-      let xn = new ComplexNumber(i/150,j/150);
+      let xn = new ComplexNumber(i/zoom,j/zoom);
       do {
         let y = f.evalulate(xn);
         let yp = fp.evalulate(xn);
         y.divide(yp);
         xn.subtraction(y);
         nxii++;
-      } while (isGreaterThanNums(xn) == null && nxii < 10);
-      let color = isGreaterThanNums(xn) || 100;
+      } while (isGreaterThanNums(xn) == null && nxii < resolution);
+      let color = isGreaterThanNums(xn) || 0;
       
       fill(color,100,100);
       rect((i+250),(j+250),1,1);
@@ -37,33 +44,19 @@ function setup() {
   }
 }
 
-var res = 0.01;
+var res = 0.5;
 
 function isGreaterThanNums(c) {
-  let st = new ComplexNumber(c.a,c.b);
-  let nd = new ComplexNumber(c.a,c.b);
-  let thd = new ComplexNumber(c.a,c.b);
-  st.subtraction(zeros.r)
-  nd.subtraction(zeros.g)
-  thd.subtraction(zeros.b)
 
-  if (st.mangnatude() <= res) {
+  if (c.dist(zeros.r) <= res) {
     
     return 0;
   }
-  if (nd.mangnatude() <= res) {
-    return 33;
-  }
-  if (thd.mangnatude() <= res) {
+  if (c.dist(zeros.g) <= res) {
     return 66;
+  }
+  if (c.dist(zeros.b) <= res) {
+    return 33;
   }
   return null;
 }
-
-
-function draw() {
-  
-}
-
-//NOTES
-//nply.difr(),100,100 is black-white gradient
